a22aee871cf999ae3786184c9b8f3419f253c39a
EgrShishov <e.shishov98@yandex.ru>
2024-07-04 12:26:08 +0300

    init repo

---
+++ 4th semester/Pz3/Pz3/Pz3.cpp
@@ -0,0 +1,170 @@
+﻿#include <iostream>
+using namespace std;
+// F[i]=A[i] * (B[i] + C[i]) - D[i] , i = 1...8.
+int main()
+{
+    __int8 A[8] = {127,-128,127,-128,127,-128,127,-128};
+    __int8 B[8] = {127,127,-128,-128,127,127,-128,-128};
+    __int8 C[8] = {127,127,127,127,-128,-128,-128,-128};
+    __int16 D[8] = {32767,32767,32767,32767,-32768,-32768,-32768,-32768};
+    __int32 F[8]{};
+
+ /*   std::cout << "Enter array of A!\n";
+    for (int i = 0; i < 8; i++) {
+        int tmp;
+        cin >> tmp;
+        A[i] = tmp;
+    }
+
+    std::cout << "Enter array of B!\n";
+    for (int i = 0; i < 8; i++) {
+        int tmp;
+        cin >> tmp;
+        B[i] = tmp;
+    }
+
+    std::cout << "Enter array of C!\n";
+    for (int i = 0; i < 8; i++) {
+        int tmp;
+        cin >> tmp;
+        C[i] = tmp;
+    }
+
+    std::cout << "Enter array of D!\n";
+    for (int i = 0; i < 8; i++) {
+        int tmp;
+        cin >> tmp;
+        D[i] = tmp;
+    }*/
+
+    __asm {
+        ; B + C
+        lea eax, B
+        lea ebx, C
+
+        movq mm0, [eax]
+        movq mm1, [ebx]
+
+        movq mm2, mm0
+        movq mm3, mm1
+
+        pxor mm4, mm4
+        pxor mm5, mm5
+
+        pcmpgtb mm4, mm2
+        pcmpgtb mm5, mm3
+
+        ; to 16 - bit
+        ; low
+        punpcklbw mm0, mm4
+        ; high
+        punpckhbw mm2, mm4
+        ; low
+        punpcklbw mm1, mm5
+        ; high
+        punpckhbw mm3, mm5
+
+
+        paddw mm0, mm1
+        paddw mm2, mm3
+        ; result in mm0, mm2
+
+        ; A* (B + C)
+        pxor mm1, mm1
+        pxor mm3, mm3
+        pxor mm4, mm4
+        pxor mm5, mm5
+        pxor mm6, mm6
+
+        movq mm1, mm0
+        movq mm3, mm2
+
+        ; converting to 32bit
+        punpcklwd mm0, mm4; low low from result
+        punpckhwd mm1, mm4; low high
+        punpcklwd mm2, mm4; high low
+        punpckhwd mm3, mm4; high high
+
+
+        lea eax, A
+        movq mm4, [eax]
+        ; extending A(low)
+        pcmpgtb mm6, mm4
+
+        punpcklbw mm4, mm6
+        movq mm5, mm4
+        pxor mm6, mm6
+        punpcklwd mm4, mm6
+        punpckhwd mm5, mm6
+
+        pmaddwd mm0, mm4
+        pmaddwd mm1, mm5
+
+        movq mm4, [eax]
+
+        ; high
+        pcmpgtb mm6, mm4
+
+        punpckhbw mm4, mm6
+        movq mm5, mm4
+        pxor mm6, mm6
+        punpcklwd mm4, mm6
+        punpckhwd mm5, mm6
+
+        pmaddwd mm2, mm4
+        pmaddwd mm3, mm5
+
+        ; result after multiplying stores in 0, 1, 2, 3
+
+
+        ; A* (B + C) - D
+        ; free registers
+        pxor mm4, mm4
+        pxor mm5, mm5
+        pxor mm6, mm6
+        pxor mm7, mm7
+
+        lea edx, D
+        movq mm4, [edx]
+        movq mm5, mm4
+
+        ; unpacking to 32bit
+        pcmpgtw mm7, mm4
+        punpcklwd mm4, mm7
+        punpckhwd mm5, mm7
+
+        psubd mm0, mm4
+        psubd mm1, mm5
+
+        pxor mm4, mm4
+        pxor mm5, mm5
+
+        ;second part
+        movq mm6, [edx + 8]
+        movq mm7, mm6
+        pcmpgtw mm4, mm6
+        punpcklwd mm6, mm4
+        punpckhwd mm7, mm4
+
+        psubd mm2, mm6
+        psubd mm3, mm7
+
+        ; result to F
+        lea eax, F
+        movq [eax], mm0
+        movq [eax + 8], mm1
+        movq [eax + 16], mm2
+        movq [eax + 24], mm3
+    }
+
+    cout << "F: ";
+    for (int i = 0; i < 8; i++) {
+        cout << std::dec << F[i] << " ";
+    }
+    std::cout << "\n\n";
+    for (int i = 0; i < 8; i++) {
+        cout << std::dec << A[i]*(B[i]+C[i])-D[i] << " ";
+    }
+
+    return 0;
+}
---
+++ 4th semester/Pz4/Pz4/Pz4.cpp
@@ -0,0 +1,120 @@
+﻿#include <iostream>
+
+using namespace std;
+// F[i]=A[i] * (B[i] + C[i]) - D[i] , i = 1...8.
+int main()
+{
+    float A[] = { 2323.676788778, 4., 6., 8., 43., 213.534, 0.2342, 543.53454};
+    float B[] = { 1., 2., 3., 4., 9.54, 20032.3544, 32.34, 121233.34};
+    float C[] = { 10., 12., 14., 16., 0.98584, 1.00003443, 9723.34, 15.435};
+    double D[] = { 127.342, 432.432, 100.001, 54.3534, 43.5435346, 10000.4, 13.54, 1234.5453};
+    double F[8]{};
+
+    /*   std::cout << "Enter array of A!\n";
+       for (int i = 0; i < 8; i++) {
+           float tmp;
+           cin >> tmp;
+           A[i] = tmp;
+       }
+
+       std::cout << "Enter array of B!\n";
+       for (int i = 0; i < 8; i++) {
+           float tmp;
+           cin >> tmp;
+           B[i] = tmp;
+       }
+
+       std::cout << "Enter array of C!\n";
+       for (int i = 0; i < 8; i++) {
+           float tmp;
+           cin >> tmp;
+           C[i] = tmp;
+       }
+
+       std::cout << "Enter array of D!\n";
+       for (int i = 0; i < 8; i++) {
+           double tmp;
+           cin >> tmp;
+           D[i] = tmp;
+       }*/
+
+    __asm {
+        lea eax, A
+        lea ebx, B
+        lea ecx, C
+        lea edx, D
+
+        ; B + C
+        cvtps2pd xmm0, [ebx]
+        cvtps2pd xmm1, [ebx + 8]
+        cvtps2pd xmm2, [ebx + 16]
+        cvtps2pd xmm3, [ebx + 24]
+
+        cvtps2pd xmm4, [ecx]
+        cvtps2pd xmm5, [ecx + 8]
+        cvtps2pd xmm6, [ecx + 16]
+        cvtps2pd xmm7, [ecx + 24]
+
+        xor ebx, ebx
+        xor ecx, ecx
+
+        addpd xmm0, xmm4
+        addpd xmm1, xmm5
+        addpd xmm2, xmm6
+        addpd xmm3, xmm7
+        ; result in xmm0, xmm1, xmm2, xmm3
+
+        xorpd xmm4, xmm4
+        xorpd xmm5, xmm5
+        xorpd xmm6, xmm6
+        xorpd xmm7, xmm7
+
+        cvtps2pd xmm4, [eax]
+        cvtps2pd xmm5, [eax + 8]
+        cvtps2pd xmm6, [eax + 16]
+        cvtps2pd xmm7, [eax + 24]
+
+        ; A* (B + C)
+        mulpd xmm0, xmm4
+        mulpd xmm1, xmm5
+        mulpd xmm2, xmm6
+        mulpd xmm3, xmm7
+        ; result in xmm0, xmm1, xmm2, xmm3
+
+        xorpd xmm4, xmm4
+        xorpd xmm5, xmm5
+        xorpd xmm6, xmm6
+        xorpd xmm7, xmm7
+
+        movupd xmm4, [edx]
+        movupd xmm5, [edx + 16]
+        movupd xmm6, [edx + 32]
+        movupd xmm7, [edx + 48]
+
+        ; A* (B + C) - D
+        subpd xmm0, xmm4
+        subpd xmm1, xmm5
+        subpd xmm2, xmm6
+        subpd xmm3, xmm7
+
+        ; result in xmm0, xmm1, xmm2, xmm3
+        lea ebx, F
+
+        movupd[ebx], xmm0
+        movupd[ebx + 16], xmm1
+        movupd[ebx + 32], xmm2
+        movupd[ebx + 48], xmm3
+    };
+
+    cout << "F: ";
+    for (int i = 0; i < 8; i++) {
+        cout << std::dec << F[i] << " ";
+    }
+    std::cout << "\n";
+    std::cout << "Right answer: ";
+    for (int i = 0; i < 8; i++) {
+        cout << std::dec << A[i] * (B[i] + C[i]) - D[i] << " ";
+    }
+
+    return 0;
+}
